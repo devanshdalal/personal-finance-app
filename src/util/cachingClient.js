011b1e52d8bcd97be1f636e774dc154bfee9f05a
Devansh D <devansh@spotnana.com>
2021-06-23 20:27:03 +0530

    loging

--- src/util/cachingClient.js
+++ src/util/cachingClient.js
@@ -7,23 +7,42 @@ const APP_HOST = process.env.REACT_APP_HOST;
 // const LSCACHE_TIMEOUT = 1; // minutes
 
 const dbClient = (options) => {
-  console.log('dbClient.options', options);
+  console.log('dbClient.options:', options);
   const { method, queryParams = {}, /*cache,*/ path = '', headers = {}, data={}, handler } = options;
 
-  try {
-    return axios({
-      method: method,
-      baseURL: APP_HOST,
-      url: path,
-      params: queryParams,
-      data: data,
-      headers
-    })
+  const p = axios({
+    method: method,
+    baseURL: APP_HOST,
+    url: path,
+    params: queryParams,
+    data: data,
+    headers
+  })
+  p.catch(err => {
+    console.log('err', err)
+    handler(err)
+  })
+  return p
+
+  // try {
+  //   const p = axios({
+  //     method: method,
+  //     baseURL: APP_HOST,
+  //     url: path,
+  //     params: queryParams,
+  //     data: data,
+  //     headers
+  //   })
+  //   p.catach(err => )
+  //   return p
     
-    // lscache.set(table, response, LSCACHE_TIMEOUT);
-  } catch (err) {
-    if(handler) handler(err)
-  }
+  //   // lscache.set(table, response, LSCACHE_TIMEOUT);
+  // } catch (err) {
+  //   if(handler) {
+  //     console.log('err', err)
+  //     handler(err)
+  //   }
+  // }
 };
 
 export default dbClient;
